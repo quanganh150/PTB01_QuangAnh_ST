22a0002a797f757da624727bc20f73a3c718cb5a
quanganh150 <quanganhnguyeenn@gmail.com>
2023-06-10 19:58:50 +0700

    commit. buoi2

---
+++ Buổi 2/buoi2.py
@@ -0,0 +1,18 @@
+#Ôn buổi 1
+# biến print: hiển thị nội dung ra màn hình
+# Các biến trong python 
+# int
+# bool
+# float
+# string
+# lệnh type()
+b = ("Python")
+print(type(b))
+a = ("Nguyễn Quang Anh")
+x = 7/8 
+y = 12
+z = 9.5
+print(a)
+print (x)
+print (y)
+pritn (z)

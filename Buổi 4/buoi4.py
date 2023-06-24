36264f8763bbc88bfb0d2d42b8e45018d9a9cbc9
quanganh150 <quanganhnguyeenn@gmail.com>
2023-06-24 20:00:42 +0700

    Update buoi4.py

---
+++ Buổi 4/buoi4.py
@@ -0,0 +1,46 @@
+#Biểu thức logic (6 loại)
+#1: lớn hơn
+#2: bé hơn
+#3: bằng
+#4: lớn hơn hoặc bằng
+#5: bé hơn hoặc bằng
+#6:
+#n = int(input("Nhập N = "))
+# điều kiện dạng khuyết
+#if (n % 2 == 0):
+ #   print("N là số chẵn")
+# điều kiện dạng đủ
+#if(n % 2 == 1):
+ #   print("N là số lẻ")
+#else:
+#    print("N là số chẵn")
+
+#import random
+#qua = random.randint(1,4)
+#if (qua == 1):
+#    print("Bỏ giỏ A.")
+#elif (qua == 2):
+#    print("Bỏ giỏ B.")
+#elif (qua == 3):
+#    print ("Bỏ giỏ C")
+#   print ("Bỏ giỏ D"):
+
+#BTVN
+# bài 1
+
+#a = int(input("Nhập A: "))
+#if (a % 2 == 0):
+ #print("A là số chẵn") 
+#else:
+ #print("A là số lẻ")
+
+#bài 2
+#dtb = int(input("Điểm trung bình môn: "))
+#if (dtb >= 9):
+  #print("Học sinh giỏi")
+#elif (dtb >=7 and dtb <= 9):
+ # print("Học sinh khá")
+#elif (dtb >= 5 and dtb <= 7):
+  #print("Học sinh trung bình")
+#else:
+ # print("Học sinh yếu")

157935e2d80dba8e521568337a5c6fa2b107a637
quanganh150 <quanganhnguyeenn@gmail.com>
2023-07-02 09:59:38 +0700

    Update buoi5.py

--- Buổi 5/buoi5.py
+++ Buổi 5/buoi5.py
@@ -7,9 +7,9 @@ b = 2
 T = int(input("Loại tài liệu: "))
 P = int(input("Số trang: "))
 C = int(input("Số bản cần in: "))
-x = 6
 if(T == 1):
    k = 1 * P * C
    print("Số lượng giấy cần dùng: " + str(k))
 if(T == 2):
-   print("Số lượng giấy cần dùng: 35")
+   k = ((P - 1) / 2 +1) * C
+   print("Số lượng giấy cần dùng: " + str(k))
